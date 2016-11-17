af6091ea055bc5608db344f64b8ae208f42d0723
Gabriel Ramos Takeda <gabrielrtakeda@gmail.com>
2016-11-17 02:12:31 -0200

    Add DisciplinaPicker component.

---
+++ src/components/DisciplinaPicker.js
@@ -0,0 +1,103 @@
+import React, { Component, PropTypes } from 'react'
+import { connect } from 'react-redux'
+import { StyleSheet, View, Picker } from 'react-native'
+import { FormLabel } from 'react-native-elements'
+
+import { Loading } from '../components'
+import { fetchDisciplinas } from '../utilities/fetchHelpers'
+import { setDisciplinaPickerSelected } from '../actions/DisciplinaActions'
+import { initialPickerItem } from '../reducers/DisciplinaReducer'
+import { isEmptyObject } from '../utilities/validationHelpers'
+
+class DisciplinaPicker extends Component {
+  componentWillMount() {
+    const { loaded, dispatch, credentials, selected } = this.props
+    let callback
+    if (!isEmptyObject(selected)) callback = list => this.setSelected(list)
+
+    if (!loaded) fetchDisciplinas({ dispatch, credentials, callback })
+    else this.setSelected(this.props.list)
+  }
+
+  setSelected(list) {
+    const { dispatch, selected } = this.props
+    const selectedIndex = list.findIndex(disciplina => disciplina.id === selected.id)
+    dispatch(setDisciplinaPickerSelected(list[selectedIndex]))
+  }
+
+  render() {
+    const {
+      list,
+      loading,
+      dispatch,
+      disciplinaPickerSelected,
+      filter,
+      selected,
+      disabled,
+    } = this.props
+
+    const disciplinas = [initialPickerItem, ...list]
+    return (
+      <View>
+        <FormLabel>Disciplina</FormLabel>
+        <View style={[styles.pickerContainer, disabled ? styles.pickerContainerDisabled : {}]}>
+          <Picker
+            style={[styles.picker, disabled ? styles.pickerDisabled : {}]}
+            selectedValue={disciplinaPickerSelected}
+            onValueChange={value => dispatch(setDisciplinaPickerSelected(value))}
+            prompt="Selecione uma disciplina..."
+            enabled={!disabled}
+          >
+            {disciplinas.filter(filter).map(disciplina => (
+              <Picker.Item
+                key={`disciplina-${disciplina.id}`}
+                label={disciplina.nome}
+                value={disciplina}
+              />
+            ))}
+          </Picker>
+        </View>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  pickerContainerDisabled: {
+    borderBottomColor: '#f2f2f2',
+  },
+  pickerContainer: {
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    borderStyle: 'solid',
+    marginHorizontal: 20,
+    marginBottom: 8,
+  },
+  picker: {
+    flex: 1,
+    height: 30,
+    color: '#86939e',
+  },
+  pickerDisabled: {
+    color: '#e2e2e2',
+  },
+})
+
+DisciplinaPicker.propTypes = {
+  filter: PropTypes.func.isRequired,
+  selected: PropTypes.object.isRequired,
+}
+
+DisciplinaPicker.defaultProps = {
+  filter: () => true
+}
+
+const mapStateToProps = state => ({
+  loading: state.disciplina.loading,
+  loaded: state.disciplina.loaded,
+  list: state.disciplina.list,
+  disciplinaPickerSelected: state.disciplina.pickerSelected,
+  credentials: state.authentication.credentials,
+})
+
+export default connect(mapStateToProps)(DisciplinaPicker)
